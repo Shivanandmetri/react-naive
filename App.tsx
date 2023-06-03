bcbe298d270b8a7709e1cfd736338d59e95546d9
Shivanandmetri <shivanand1998metri@gmail.com>
2023-06-03 12:33:03 +0530

    WeatherApp

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
-import 'react-native-gesture-handler';
-import SignUp from './components/FigmaTask/SignUp';
+import {Text, View} from 'react-native';
 import React from 'react';
-import Stacknavigation from './components/FigmaTask/stacknavigation';
-import Login from './components/FigmaTask/Login';
-import Scanned from './components/FigmaTask/Scanned';
 
 const App = () => {
   return (
-    // <Login />
-    // <Scanned />
-    <Stacknavigation />
+    <View>
+      <Text>Hello world</Text>
+    </View>
   );
 };
 
